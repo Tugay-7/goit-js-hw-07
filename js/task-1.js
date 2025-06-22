49618db28db3da8852c1cf8c071d1a104575ff88
Tugay-7 <tugayozdes@gmail.com>
2025-06-22 17:18:07 +0300

    Fix syntax error in querySelector and corrected console log format

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,9 @@ const categories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((element) => {
-    const categoryName = element.querySelector(h2)?.textContent || 'Unknown';
+    const categoryName = element.querySelector('h2')?.textContent || 'Unknown';
     const numberOfElements = element.querySelectorAll('ul li').length;
 
-    console.log(`category): ${categoryName}`);
+    console.log(`category: ${categoryName}`);
     console.log(`Elements: ${numberOfElements}`);
 });
