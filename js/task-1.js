0f3f938f6af3df77e9820580d6ee08407630c6e1
Tugay-7 <tugayozdes@gmail.com>
2025-06-22 14:31:16 +0300

    js-hw-07

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categories = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((element) => {
+    const categoryName = element.querySelector(h2)?.textContent || 'Unknown';
+    const numberOfElements = element.querySelectorAll('ul li').length;
+
+    console.log(`category): ${categoryName}`);
+    console.log(`Elements: ${numberOfElements}`);
+});
